bd82fc429f1019935e0ad022276caea2d3c150ce
vexy <vexy1337@gmail.com>
2023-08-26 15:42:25 +0300

    added main.h

---
+++ Code/inc/main.h
@@ -0,0 +1,26 @@
+#pragma once
+
+
+#define DEBUG_OPTIONS TRUE
+
+
+#include <stdint.h>
+#include <atomic>
+#include <stdlib.h>
+#include <string.h>
+
+#include "ch.hpp"
+#include "hal.h"
+
+#include "shell.h"
+#include "chprintf.h"
+
+
+#include "rt_test_root.h"
+#include "oslib_test_root.h"
+#include "usbcfg.h"
+#include "ringBuffer.hpp"
+
+#include "threads.h"
+
+#include "chdebug.h"
